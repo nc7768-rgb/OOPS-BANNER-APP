8fcd67d4bacf977720f6df209ededb0a156ced8f
nc7768-rgb <nc7768@srmist.edu.in>
2026-03-02 20:50:45 +0530

    UC2 Render OOPS as Banner using Print statements

--- OOPS-BANNER-APP.java
+++ OOPS-BANNER-APP.java
@@ -1,17 +1,20 @@
-gi/**
+/**
  * OOPSBannerApp
- * 
- * Prints the literal text "OOPS" to the console.
- * 
+ * Prints OOPS in large banner format
  * @author NIKHIL CHANDRAN
- * @version 1.0
+ * @version 2.0
  */
 class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****   *****  ");
+        System.out.println("*     * *     * *            * ");
+        System.out.println("*     * *     * *            * ");
+        System.out.println(" *****   *****  *       ****** ");
+
     }
 }
